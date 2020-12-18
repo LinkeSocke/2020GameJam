d4c884610d9462a7a39b942b3b033c787438129a
Mario Kerndler <mariokerndler@gmail.com>
2020-12-18 22:38:54 +0100

    Basic Movment
    Move, Crouch, Jump

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private PlayerMovement moveController = null;
+    [SerializeField] private float movementSpeed = 40f;
+
+    private float movementXAxis = 0f;
+    private bool jump = false;
+    private bool crouch = false;
+    
+    private void Awake()
+    {
+        moveController = GetComponent<PlayerMovement>();
+    }
+
+    private void FixedUpdate()
+    {
+        moveController.Move(movementXAxis, crouch, jump);
+        jump = false;
+    }
+
+    public void Movement(InputAction.CallbackContext context)
+    {
+        // Read Movement Vector
+        movementXAxis = context.ReadValue<Vector2>().x * movementSpeed * Time.deltaTime;
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        jump = true;
+    }
+
+    public void Crouch(InputAction.CallbackContext context)
+    {
+        if (crouch) crouch = false;
+        else crouch = true;
+    }
+}
