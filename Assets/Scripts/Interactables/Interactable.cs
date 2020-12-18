2f0f9b4da4c7c4af2c74d44b3a2de6ec85220f7b
Mario Kerndler <mariokerndler@gmail.com>
2020-12-18 23:41:17 +0100

    Basic Interaction System

---
+++ Assets/Scripts/Interactables/Interactable.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interactable : MonoBehaviour, IInteractable
+{
+    public UnityEvent OnInteract;
+
+    private void Awake()
+    {
+        if (OnInteract == null)
+            OnInteract = new UnityEvent();
+    }
+
+    public void Interact()
+    {
+        OnInteract.Invoke();
+    }
+
+}
