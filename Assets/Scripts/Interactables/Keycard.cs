0bab68961ac2e5b13647e457dc199d74137d8448
Mario Kerndler <mariokerndler@gmail.com>
2020-12-19 00:52:24 +0100

    Keycard and Door

--- Assets/Scripts/Interactables/Keycard.cs
+++ Assets/Scripts/Interactables/Keycard.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public enum EKeycardType { BLUE, RED, GREEN }
 
+[CreateAssetMenu(fileName = "Keycard", menuName = "ScriptableObjects/Keycard", order = 1)]
 public class Keycard : ScriptableObject
 {
     public EKeycardType keycardType = EKeycardType.BLUE;
+    public Color keycardColor = Color.blue;
 }
---
+++ Assets/Scripts/Interactables/KeycardPickup.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Interactable))]
+public class KeycardPickup : MonoBehaviour
+{
+    [SerializeField] private Keycard keycard = null;
+
+    private void Start()
+    {
+        var sprite = GetComponent<SpriteRenderer>();
+        sprite.color = keycard.keycardColor;
+    }
+
+    public void Pickup()
+    {
+        var player = GameObject.FindGameObjectWithTag("Player");
+        if (player == null) return;
+
+        var playerController = player.GetComponent<PlayerController>();
+        if (playerController == null) return;
+
+        playerController.AddKeycard(keycard);
+
+        Destroy(this.gameObject);
+    }
+
+}
