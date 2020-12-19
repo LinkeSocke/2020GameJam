e9248ed7e8b1fc38e2b768cc81e85f540ef7922b
Mocca101 NB <49754596+Mocca101@users.noreply.github.com>
2020-12-19 01:42:01 +0100

    Inital Fuzebox Action Setup

---
+++ Assets/Scripts/Fuzebox/FuzeboxActions/FuzeboxAction.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DefaultFuzeboxAction", menuName = "ScriptableObjects/DefaultFuzeboxAction", order = 1)]
+public class FuzeboxAction : ScriptableObject, IFuzeboxAction
+{
+    [SerializeField]
+    private string actionValue = "No Action";
+
+    public string GetValue()
+    {
+        return actionValue;
+    }
+}
---
+++ Assets/Scripts/Fuzebox/FuzeboxActions/FuzeboxActionSequence.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Malee.List;
+
+[CreateAssetMenu(fileName = "FuzeboxActionSequence", menuName = "ScriptableObjects/FuzeboxActionSequence", order = 1)]
+public class FuzeboxActionSequence : ScriptableObject
+{
+ 
+        [Reorderable]
+        public FuzeboxActionList fuzeboxActionSequence;
+
+
+}
