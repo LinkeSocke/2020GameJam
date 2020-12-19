d75a90086da0bf109f573d672cb296e8cd19d886
Mario Kerndler <mariokerndler@gmail.com>
2020-12-20 00:49:26 +0100

    Auto stash before merge of "CameraMask" and "origin/moreAssets"

---
+++ Assets/Scripts/Manager/Events/MaskEvent.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaskEvent : MonoBehaviour, IGameEvent
+{
+    public void Invoke()
+    {
+        var maskList = GameObject.FindObjectsOfType<SpriteMask>();
+    }
+}
