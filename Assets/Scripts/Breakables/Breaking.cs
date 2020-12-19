bfa551dc9c4871b43ea4d7a0f830f6fa6a46c604
David Holy <davidholy@me.com>
2020-12-19 04:21:57 +0100

    Different Collision Detection Scripts with Unity Events

---
+++ Assets/Scripts/Breakables/Breaking.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breaking : MonoBehaviour, IBreakable
+{
+    [SerializeField]
+    protected GameObject wholeObject;
+
+    [SerializeField]
+    protected GameObject brokenObject;
+
+    protected bool broken = false;
+
+    public virtual void Break()
+    {
+        if (!broken)
+        {
+            broken = true;
+
+            Rigidbody2D body = GetComponent<Rigidbody2D>();
+            if (body)
+            {
+                Destroy(body);
+            }
+
+            var colliders = GetComponents<Collider2D>();
+            foreach(var collider in colliders)
+            {
+                Destroy(collider);
+            }
+
+            Destroy(wholeObject);
+            brokenObject.SetActive(true);
+
+            System.Random rdm = new System.Random();
+
+            var children = brokenObject.transform.GetComponentsInChildren<Transform>();
+            foreach (var child in children)
+            {
+                Destroy(child.gameObject, rdm.Next(5, 10));
+            }
+
+            Destroy(this.gameObject, 15);
+        }
+    }
+}
---
+++ Assets/Scripts/Breakables/StompDetection.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class StompDetection : MonoBehaviour
+{
+    [SerializeField]
+    private int durability = 1;
+
+    [SerializeField]
+    public UnityEvent onStomp;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" || collision.gameObject.GetComponent<IBreakable>() != null)
+        {
+            durability--;
+            if (durability <= 0)
+            {
+                onStomp?.Invoke();
+            }
+        }
+    }
+}
