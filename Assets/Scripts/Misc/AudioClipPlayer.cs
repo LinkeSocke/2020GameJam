11b76977771f7d4dfb5d310dee795bbfe16e3282
Mario Kerndler <mariokerndler@gmail.com>
2020-12-20 12:57:52 +0100

    Audio Scriptable Object

---
+++ Assets/Scripts/Misc/AudioClipPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioClipPlayer : MonoBehaviour
+{
+    [SerializeField] private AudioCollection collection = null;
+    private AudioSource source;
+
+    private void Start()
+    {
+        source = GetComponent<AudioSource>();   
+    }
+
+    public void PlayRandomClip()
+    {
+        if (collection.audioClips.IsNullOrEmpty()) return;
+
+        int rand = Mathf.RoundToInt(Random.Range(0f, (float)collection.audioClips.Count - 1));
+        source.PlayOneShot(collection.audioClips[rand]);
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/Keycard.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EKeycardType { BLUE, RED, GREEN }
+
+[CreateAssetMenu(fileName = "Keycard", menuName = "ScriptableObjects/Keycard", order = 1)]
+public class Keycard : ScriptableObject
+{
+    public EKeycardType keycardType = EKeycardType.BLUE;
+    public Color keycardColor = Color.blue;
+    public Sprite doorSprite = null;
+    public Sprite keycardSprite = null;
+}
